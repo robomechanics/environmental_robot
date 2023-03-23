40ef43a3ef32829116c3f26982b3929e280ec5da
slothyshane <shanedeng@cmu.edu>
2023-03-23 14:53:51 -0400

    changed the obstacle map

--- autonomy_manager/src/obsAvoidance.py
+++ autonomy_manager/src/obsAvoidance.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python3
 import rospy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
 from std_srvs.srv import SetBool, SetBoolRequest
 from std_msgs.msg import Float64, Header
 from nav_msgs.msg import Odometry, OccupancyGrid
 import numpy as np
 import matplotlib as plt 
+f
 from sensor_msgs.msg import PointCloud2
 import sensor_msgs.point_cloud2 as pc2
 
@@ -13,6 +14,9 @@ class obsAvoidance(object):
 	def __init__(self):
 		rospy.init_node('obstacleAvoidance', anonymous=True)
 		rospy.Subscriber('/ouster/points', PointCloud2, self.obsMap)
+		self.cost_map_pub = rospy.Publisher('/costmap', OccupancyGrid, queue_size=1)
+		rospy.Subscriber('/utm_odom2', Odometry, self.odomCallback)
+
 		#define class variables
 		self.heightThreshLow = -0.4
 		self.heightThreshHigh = 0.1
@@ -25,14 +29,45 @@ class obsAvoidance(object):
 		
 		#discretization
 		self.cell = 0.05
+		self.threshold = 2
 
 		# map size
 		self.mapx = self.x / self.cell
 		self.mapy = self.y / self.cell
 
+		#pose
+		self.pose = Pose()
+		self.time = None 
 		#rate = rospy.Rate(10)
 		#rate.sleep()
-		rospy.spin()		
+		rospy.spin()	
+
+	def odomCallback(self, data):
+		self.pose = data.pose
+		self.time = data.header.stamp
+		# get the current position of the robot
+
+	def create_occupancy_grid(self, cost_map_2d):
+		grid = OccupancyGrid()
+		
+		# Customize the header information
+		grid.header = Header()
+		grid.header.stamp = self.time
+		grid.header.frame_id = "costmap_frame"
+		
+		# Set the properties of the OccupancyGrid
+		grid.info.resolution = 0.1  # resolution in meters
+		grid.info.width = len(cost_map_2d[0])
+		grid.info.height = len(cost_map_2d)
+		grid.info.origin.pose = self.pose
+		
+		flat_map = [cell for row in cost_map_2d for cell in row]
+		
+		# Assign the flattened cost map to the OccupancyGrid data
+		grid.data = flat_map
+
+		return grid
+	
 	def obsMap(self, data):
 		env = []
 		localMap = np.full((self.mapx, self.mapy), 0)
@@ -48,16 +83,20 @@ class obsAvoidance(object):
 				continue
 
 			localMap[round(point[0] / self.cell)][round(point[1] / self.cell)] += 1
+			# print("x:{}, y:{}, z:{}".format(point[0], point[1], point[2]))
+		costMap = (localMap > self.threshold).astype(int)
 
+		og = self.create_occupancy_grid(costMap)
+		self.cost_map_pub.publish(og)
 
-			print("x:{}, y:{}, z:{}".format(point[0], point[1], point[2]))
-			env.append(env)
-		#env = np.asarray(env)
-		#print(env.shape)	
 			
+		#env.append(env)
+	
 if __name__ == '__main__':
-	obsAvoidance()
-
+	try:
+		obsAvoidance()
+	except rospy.ROSInterruptException:
+		pass
 
 
 
