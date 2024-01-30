c1adce07d1a5d55345fca27f8fbfa0eb93ef1b25
proboticks <proboticks@fry.tail23bb6.ts.net>
2024-01-29 21:05:07 -0500

    Remove old code
    Renamed autonomy_teleop_instant to autonomy_teleop in autonomy_manager
    Renamed controls2.launch to controls.launch in autonomy_manager
    Removed kraton, camera and rake_scan pkgs
    Removed gps_navigation_rviz, gps_navigation, test, rake_measure and scripts folder in gps_navigation pkg
    
    Deprecated: curve_graph, dataParser, dummy_services, test, test_adaptive

---
+++ autonomy_manager/deprecated/dummy_services.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import rospy
+from autonomy_manager.srv import DeployAutonomy,NavigateGPS, RunSensorPrep
+from std_srvs.srv import SetBool
+class dummy_search(object):
+    def __init__(self,boundary):
+        self.boundary = boundary
+    def next(self):
+        return [0,0]
+    def update(self,data):
+        pass
+
+class dummy_services(object):
+    def __init__(self):
+        rospy.init_node('dummy_services', anonymous=True)
+        rospy.Service('/run_sensor_prep', RunSensorPrep,self.dummy)
+        rospy.Service('/next_goal_nav', NavigateGPS,self.dummy)
+        rospy.Service('/deploy_sensor', SetBool, self.dummy2)
+        rospy.Service('/start', SetBool, self.dummy2)
+        rospy.spin()
+
+    def dummy(self,data):
+        #print(data)
+        return True
+
+    def dummy2(self,data):
+        #print(data)
+        return (True, "")
+if __name__ == '__main__':
+    dummy_services()
---
+++ autonomy_manager/deprecated/test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append('../')
+from boundaryConversion import *
+
+# 40.44236709308751, -79.94600177667006
+# x, y = utm(40.44236709308751, -79.94600177667006)
+# print(x)
+# print(y)
+
+#[40.44208352622619, -79.94610353523909, 1]
+#[40.44236251851889, -79.94599539848328, 1]
+#[40.44233453704325, -79.94584400702514, 1]
+#[40.442054721646244, -79.9459456555756, 1]
+
+boundary_point = [[40.44208352622619, -79.94610353523909, 1], [40.44236251851889, -79.94599539848328, 1], [40.44233453704325, -79.94584400702514, 1], [40.442054721646244, -79.9459456555756, 1]]
+
+conv = Conversion()
+
+conv.get_zone(40.44208352622619, -79.94610353523909)
+for each in boundary_point:
+    lat, lon = conv.get_utm(each[0], each[1])
+    print('-----------------')
+    print(lat)
+    print(lon)
+    print('-----------------')
+
+print('----Map dimension----')
+width, height = conv.boundaryConversion(boundary_point)
+print(width)
+print(height)
+
+print('---origin---')
+print(conv.origin_utm)
+
+
+lat, lon = conv.map2gps(11, 17)
+print(lat)
+print(lon)
+
+east, north =  conv.get_utm(40.44208352622619, -79.94610353523909)
+print(east)
+print(north)
+
+lat, lon = conv.get_gps(east, north)
+print(lat)
+print(lon)
+# x, y = conv.get_utm(40.44236709308751, -79.94600177667006)
+# lon, lat = conv.get_gps(x, y)
+# print(lon)
+# print(lat)
