d1c8b830029333fdf890ca0c0b1667655c66d5b9
Vivek T <vthangav@cmu.edu>
2024-04-18 14:24:10 -0400

    Added EKF Status Check and removed odom initial

--- robomech_nav/scripts/gps_odom_pub.py
+++ robomech_nav/scripts/gps_odom_pub.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 import rospy
 import numpy as np
-
-from pyproj import CRS, Transformer
-
 import tf
 from tf.transformations import euler_from_quaternion
 import tf2_ros
 import geometry_msgs.msg
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import NavSatFix
+from microstrain_inertial_msgs.msg import HumanReadableStatus
+import message_filters
+from pyproj import Transformer
 
 class UTMToOdom():
     def __init__(self, parent_frame_id="base_link", child_frame_id="utm_odom"):
-        self.first_time_LatLon = True
+        self.first_LatLon = True
+        self.is_full_nav_achieved = False
         # self.first_time_Orientation = True
+        
+        self.load_ros_params()
         
         self.crs_GPS = 'EPSG:4326'
         self.crs_UTM = 'EPSG:3364'
         self.transformer = Transformer.from_crs(self.crs_GPS, self.crs_UTM)
 
-        self.pose_odom_initial = geometry_msgs.msg.Pose()
         self.pose_odom_covariance_initial = geometry_msgs.msg.PoseWithCovariance()
         self.pose_odom = geometry_msgs.msg.Pose()
         self.pose_odom_covariance = geometry_msgs.msg.PoseWithCovariance()
 
         self.quaternion = tf.transformations.random_quaternion()
 
-        self.odom_initial = Odometry()
         self.odom = Odometry()
 
         # Creating a transform and its broadcaster
@@ -38,17 +39,42 @@ class UTMToOdom():
         self.t.child_frame_id = child_frame_id
 
 		# Publishers and Subscribers
-        self.utm_odom_pub = rospy.Publisher("utm_odom", Odometry, queue_size=1)
-        self.utm_odom_initial_pub = rospy.Publisher("utm_odom_initial", Odometry, queue_size=1)
-        self.orientation_sub = rospy.Subscriber("/gq7/ekf/odometry_map", Odometry, self.updateOrientation)
-        self.latlon_sub = rospy.Subscriber("/gq7/ekf/llh_position", NavSatFix, self.utm_broadcaster)
-        self.atenna1_sub = rospy.Subscriber("/gq7/gnss_1/llh_position", NavSatFix, self.antenna1_update)
-        self.atenna2_sub = rospy.Subscriber("/gq7/gnss_2/llh_position", NavSatFix, self.antenna2_update)
+        self.utm_odom_pub = rospy.Publisher(self._tf_utm_odom_topic, Odometry, queue_size=1)
 
-    def updateOrientation(self,data):
-        self.updateOrientationVals(data)
+        self.status_sub = rospy.Subscriber(self._gq7_ekf_status_topic, HumanReadableStatus, self.status_callback)
+        
+        self.ekf_odom_map_sub = message_filters.Subscriber(self._gq7_ekf_odom_map_topic, Odometry)
+        self.ekf_llh_sub = message_filters.Subscriber(self._gq7_ekf_llh_topic, NavSatFix)
 
-    def updateOrientationVals(self,data):
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.ekf_odom_map_sub , self.ekf_llh_sub], 1, True, True)
+        self.ts.registerCallback(self.time_sync_callback)
+        
+        # self.atenna1_sub = rospy.Subscriber("/gq7/gnss_1/llh_position", NavSatFix, self.antenna1_update)
+        # self.atenna2_sub = rospy.Subscriber("/gq7/gnss_2/llh_position", NavSatFix, self.antenna2_update)
+    
+    def load_ros_params(self):
+        # Load topic names into params
+        self._tf_base_link_frame = rospy.get_param('tf_base_link_frame')
+        self._tf_utm_odom_frame = rospy.get_param('tf_utm_odom_frame')
+        self._tf_utm_odom_topic = rospy.get_param('tf_utm_odom_topic')
+        self._gq7_ekf_odom_map_topic = rospy.get_param('gq7_ekf_odom_map_topic')
+        self._gq7_ekf_llh_topic = rospy.get_param('gq7_ekf_llh_topic')
+        self._gq7_ekf_status_topic = rospy.get_param('gq7_ekf_status_topic')
+        
+    def status_callback(self, data):
+        # todo: check data.status_flags.heading_warning?
+        if ((self.is_full_nav_achieved == False) and
+            (data.filter_state == '"Full Nav"') and
+            ('"Stable"' in data.status_flags) ):
+            self.is_full_nav_achieved = True
+            rospy.loginfo ("FULL NAV ACHIEVED")
+            
+    def time_sync_callback(self, odom_map_data, llh_data):
+        if self.is_full_nav_achieved:
+            self.update_orientation(odom_map_data)
+            self.utm_broadcaster(llh_data)
+                
+    def update_orientation(self,data):
         # Grabbing the quaternion orientation
         self.quaternion[0] = data.pose.pose.orientation.x
         self.quaternion[1] = data.pose.pose.orientation.y
@@ -57,40 +83,30 @@ class UTMToOdom():
 
     def utm_broadcaster(self, data):
         self.odom = Odometry()
-        self.firstTimeCode(data)
-        self.utm_odom_initial_pub.publish(self.odom_initial)
-        self.handleGPS_MSG(data)
+        if self.first_LatLon is True:
+            self.get_origin(data)
+        self.handle_GPS_MSG(data)
         self.create_UTM_Odom(data)
-        self.findInverses(data)
-        self.sendBackTF(data)
-        self.backCalcAntennas(data)
+        self.find_inverses(data)
+        self.send_back_TF(data)
+        self.back_calc_antennas(data)
 
-    def firstTimeCode(self,data):
-        if self.first_time_LatLon is True:
-            # Grab initial latlon values
-            self.lat_GPS_start = data.latitude
-            self.lon_GPS_start = data.longitude
+    def get_origin(self,data):
+        # Grab initial latlon values
+        self.lat_GPS_start = data.latitude
+        self.lon_GPS_start = data.longitude
 
-            # Convert from lat-lon to UTM using py proj
-            self.XY_UTM_start = self.transformer.transform(self.lat_GPS_start, self.lon_GPS_start)
-            self.x_UTM_start = self.XY_UTM_start[0]
-            self.y_UTM_start = self.XY_UTM_start[1]
+        # Convert from lat-lon to UTM using py proj
+        self.XY_UTM_start = self.transformer.transform(self.lat_GPS_start, self.lon_GPS_start)
+        self.x_UTM_start = self.XY_UTM_start[0]
+        self.y_UTM_start = self.XY_UTM_start[1]
 
-            self.first_time_LatLon = False
+        self.first_LatLon = False
+        
+        self.status_sub.unregister()
+        
 
-            # Creating a pose message for location in Odom
-            self.pose_odom_initial.position.x = self.x_UTM_start
-            self.pose_odom_initial.position.y = self.y_UTM_start
-            self.pose_odom_initial.position.z = 0.0
-            self.pose_odom_covariance_initial.pose = self.pose_odom_covariance_initial
-
-            # Formatting the Odom message
-            self.odom_initial.header.stamp = data.header.stamp
-            self.odom_initial.header.frame_id = "utm_odom"
-            self.odom_initial.child_frame_id = "base_link"
-            self.odom_initial.pose = self.pose_odom_covariance_initial
-
-    def handleGPS_MSG(self,data):
+    def handle_GPS_MSG(self,data):
         # Grab latlon values
         self.lat_GPS = data.latitude
         self.lon_GPS = data.longitude
@@ -121,7 +137,7 @@ class UTMToOdom():
         self.odom.child_frame_id = "base_link"
         self.odom.pose = self.pose_odom_covariance
 
-    def findInverses(self,data):
+    def find_inverses(self,data):
         # Publishing the Odom message
         self.utm_odom_pub.publish(self.odom)
 
@@ -135,7 +151,7 @@ class UTMToOdom():
         self.xBack = self.gInv[0][3]
         self.yBack = self.gInv[1][3]
 
-    def sendBackTF(self,data):
+    def send_back_TF(self,data):
         # Defining the backwards transformation
         self.t.header.stamp = data.header.stamp
 
@@ -152,7 +168,7 @@ class UTMToOdom():
 
         self.br.sendTransform(self.t)
 
-    def backCalcAntennas(self,data):
+    def back_calc_antennas(self,data):
         a = 0
 
     def antenna1_update(self,data):
@@ -188,9 +204,6 @@ class UTMToOdom():
         self.delta = self.b2 - self.b1
 
         print(self.delta)
-
-
-
 
 if __name__ == '__main__':
     rospy.init_node('utm_to_odom_node', anonymous=True)
