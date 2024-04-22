6c5067265ee77527b2aa519003ce97a7363df246
proboticks <you@example.com>
2024-04-22 19:40:13 -0400

    Fix GPS Odom
    Added UTM config to constant.yaml
    Format gps_odom_pub and teleop_patrick
    Tuned navstack params
    Publish cmd_vel from joystick only when R1 is pressed

--- robomech_nav/scripts/gps_odom_pub.py
+++ robomech_nav/scripts/gps_odom_pub.py
@@ -11,17 +11,16 @@ from microstrain_inertial_msgs.msg import HumanReadableStatus
 import message_filters
 from pyproj import Transformer
 
-class UTMToOdom():
-    def __init__(self, parent_frame_id="base_link", child_frame_id="utm_odom"):
+
+class UTMToOdom:
+    def __init__(self):
         self.first_LatLon = True
         self.is_full_nav_achieved = False
         # self.first_time_Orientation = True
-        
+
         self.load_ros_params()
-        
-        self.crs_GPS = 'EPSG:4326'
-        self.crs_UTM = 'EPSG:3364'
-        self.transformer = Transformer.from_crs(self.crs_GPS, self.crs_UTM)
+
+        self.transformer = Transformer.from_crs(self._crs_GPS, self._crs_UTM)
 
         self.pose_odom_covariance_initial = geometry_msgs.msg.PoseWithCovariance()
         self.pose_odom = geometry_msgs.msg.Pose()
@@ -35,46 +34,61 @@ class UTMToOdom():
         self.br = tf2_ros.TransformBroadcaster()
 
         self.t = geometry_msgs.msg.TransformStamped()
-        self.t.header.frame_id = parent_frame_id
-        self.t.child_frame_id = child_frame_id
+        self.t.header.frame_id = self._tf_base_link_frame
+        self.t.child_frame_id = self._tf_utm_odom_frame
 
-		# Publishers and Subscribers
-        self.utm_odom_pub = rospy.Publisher(self._tf_utm_odom_topic, Odometry, queue_size=1)
+        # Publishers and Subscribers
+        self.utm_odom_pub = rospy.Publisher(
+            self._tf_utm_odom_topic, Odometry, queue_size=1
+        )
 
-        self.status_sub = rospy.Subscriber(self._gq7_ekf_status_topic, HumanReadableStatus, self.status_callback)
-        
-        self.ekf_odom_map_sub = message_filters.Subscriber(self._gq7_ekf_odom_map_topic, Odometry)
-        self.ekf_llh_sub = message_filters.Subscriber(self._gq7_ekf_llh_topic, NavSatFix)
+        self.status_sub = rospy.Subscriber(
+            self._gq7_ekf_status_topic, HumanReadableStatus, self.status_callback
+        )
 
-        self.ts = message_filters.ApproximateTimeSynchronizer([self.ekf_odom_map_sub , self.ekf_llh_sub], 1, True, True)
+        self.ekf_odom_map_sub = message_filters.Subscriber(
+            self._gq7_ekf_odom_map_topic, Odometry
+        )
+        self.ekf_llh_sub = message_filters.Subscriber(
+            self._gq7_ekf_llh_topic, NavSatFix
+        )
+
+        self.ts = message_filters.ApproximateTimeSynchronizer(
+            [self.ekf_odom_map_sub, self.ekf_llh_sub], 1, True, True
+        )
         self.ts.registerCallback(self.time_sync_callback)
-        
+
         # self.atenna1_sub = rospy.Subscriber("/gq7/gnss_1/llh_position", NavSatFix, self.antenna1_update)
         # self.atenna2_sub = rospy.Subscriber("/gq7/gnss_2/llh_position", NavSatFix, self.antenna2_update)
-    
+
     def load_ros_params(self):
         # Load topic names into params
-        self._tf_base_link_frame = rospy.get_param('tf_base_link_frame')
-        self._tf_utm_odom_frame = rospy.get_param('tf_utm_odom_frame')
-        self._tf_utm_odom_topic = rospy.get_param('tf_utm_odom_topic')
-        self._gq7_ekf_odom_map_topic = rospy.get_param('gq7_ekf_odom_map_topic')
-        self._gq7_ekf_llh_topic = rospy.get_param('gq7_ekf_llh_topic')
-        self._gq7_ekf_status_topic = rospy.get_param('gq7_ekf_status_topic')
-        
+        self._tf_base_link_frame = rospy.get_param("tf_base_link_frame")
+        self._tf_utm_odom_frame = rospy.get_param("tf_utm_odom_frame")
+        self._tf_utm_odom_topic = rospy.get_param("tf_utm_odom_topic")
+        self._gq7_ekf_odom_map_topic = rospy.get_param("gq7_ekf_odom_map_topic")
+        self._gq7_ekf_llh_topic = rospy.get_param("gq7_ekf_llh_topic")
+        self._gq7_ekf_status_topic = rospy.get_param("gq7_ekf_status_topic")
+        self._crs_GPS = rospy.get_param("crs_GPS")
+        self._crs_UTM = rospy.get_param("crs_UTM")
+
     def status_callback(self, data):
         # todo: check data.status_flags.heading_warning?
-        if ((self.is_full_nav_achieved == False) and
-            (data.filter_state == '"Full Nav"') and
-            ('"Stable"' in data.status_flags) ):
+        if (
+            (self.is_full_nav_achieved == False)
+            and (data.filter_state == '"Full Nav"')
+            and ('"Stable"' in data.status_flags)
+        ):
             self.is_full_nav_achieved = True
-            rospy.loginfo ("FULL NAV ACHIEVED")
-            
+            rospy.loginfo("FULL NAV ACHIEVED")
+
     def time_sync_callback(self, odom_map_data, llh_data):
+        # rospy.loginfo("Sync Callback")
         if self.is_full_nav_achieved:
             self.update_orientation(odom_map_data)
             self.utm_broadcaster(llh_data)
-                
-    def update_orientation(self,data):
+
+    def update_orientation(self, data):
         # Grabbing the quaternion orientation
         self.quaternion[0] = data.pose.pose.orientation.x
         self.quaternion[1] = data.pose.pose.orientation.y
@@ -89,24 +103,24 @@ class UTMToOdom():
         self.create_UTM_Odom(data)
         self.find_inverses(data)
         self.send_back_TF(data)
-        self.back_calc_antennas(data)
 
-    def get_origin(self,data):
+    def get_origin(self, data):
         # Grab initial latlon values
         self.lat_GPS_start = data.latitude
         self.lon_GPS_start = data.longitude
 
         # Convert from lat-lon to UTM using py proj
-        self.XY_UTM_start = self.transformer.transform(self.lat_GPS_start, self.lon_GPS_start)
+        self.XY_UTM_start = self.transformer.transform(
+            self.lat_GPS_start, self.lon_GPS_start
+        )
         self.x_UTM_start = self.XY_UTM_start[0]
         self.y_UTM_start = self.XY_UTM_start[1]
 
         self.first_LatLon = False
-        
-        self.status_sub.unregister()
-        
 
-    def handle_GPS_MSG(self,data):
+        self.status_sub.unregister()
+
+    def handle_GPS_MSG(self, data):
         # Grab latlon values
         self.lat_GPS = data.latitude
         self.lon_GPS = data.longitude
@@ -117,10 +131,10 @@ class UTMToOdom():
         self.y_UTM = self.XY_UTM[1]
 
         # Grabbing a heading angle
-        eulerVals = euler_from_quaternion(self.quaternion, 'sxyz')
+        eulerVals = euler_from_quaternion(self.quaternion, "sxyz")
         self.theta_heading = eulerVals[2]
 
-    def create_UTM_Odom(self,data):
+    def create_UTM_Odom(self, data):
         # Creating a pose message for location in Odom
         self.pose_odom.position.x = self.x_UTM - self.x_UTM_start
         self.pose_odom.position.y = self.y_UTM - self.y_UTM_start
@@ -137,21 +151,35 @@ class UTMToOdom():
         self.odom.child_frame_id = "base_link"
         self.odom.pose = self.pose_odom_covariance
 
-    def find_inverses(self,data):
+    def find_inverses(self, data):
         # Publishing the Odom message
         self.utm_odom_pub.publish(self.odom)
 
         # Inverse transformation matrix
-        self.g = np.array([[np.cos(self.theta_heading), -np.sin(self.theta_heading), 0, (self.x_UTM - self.x_UTM_start)], 
-                      [np.sin(self.theta_heading),  np.cos(self.theta_heading), 0, (self.y_UTM - self.y_UTM_start)],
-                      [0.0, 0.0, 1.0, 0.0],
-                      [0.0, 0.0, 0.0, 1.0]])
+        self.g = np.array(
+            [
+                [
+                    np.cos(self.theta_heading),
+                    -np.sin(self.theta_heading),
+                    0,
+                    (self.x_UTM - self.x_UTM_start),
+                ],
+                [
+                    np.sin(self.theta_heading),
+                    np.cos(self.theta_heading),
+                    0,
+                    (self.y_UTM - self.y_UTM_start),
+                ],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0],
+            ]
+        )
         self.gInv = tf.transformations.inverse_matrix(self.g)
 
         self.xBack = self.gInv[0][3]
         self.yBack = self.gInv[1][3]
 
-    def send_back_TF(self,data):
+    def send_back_TF(self, data):
         # Defining the backwards transformation
         self.t.header.stamp = data.header.stamp
 
@@ -168,10 +196,7 @@ class UTMToOdom():
 
         self.br.sendTransform(self.t)
 
-    def back_calc_antennas(self,data):
-        a = 0
-
-    def antenna1_update(self,data):
+    def antenna1_update(self, data):
         self.lat1 = data.latitude
         self.lon1 = data.longitude
 
@@ -179,14 +204,14 @@ class UTMToOdom():
         self.x1 = self.xy1[0]
         self.y1 = self.xy1[1]
 
-        self.v1 = np.zeros((4,1))
-        self.v1[0,0] = self.x1
-        self.v1[1,0] = self.y1
-        self.v1[3,0] = 1.0
+        self.v1 = np.zeros((4, 1))
+        self.v1[0, 0] = self.x1
+        self.v1[1, 0] = self.y1
+        self.v1[3, 0] = 1.0
 
         self.b1 = self.gInv @ self.v1
 
-    def antenna2_update(self,data):
+    def antenna2_update(self, data):
         self.lat2 = data.latitude
         self.lon2 = data.longitude
 
@@ -194,10 +219,10 @@ class UTMToOdom():
         self.x2 = self.xy2[0]
         self.y2 = self.xy2[1]
 
-        self.v2 = np.zeros((4,1))
-        self.v2[0,0] = self.x2
-        self.v2[1,0] = self.y2
-        self.v2[3,0] = 1.0
+        self.v2 = np.zeros((4, 1))
+        self.v2[0, 0] = self.x2
+        self.v2[1, 0] = self.y2
+        self.v2[3, 0] = 1.0
 
         self.b2 = self.gInv @ self.v2
 
@@ -205,8 +230,9 @@ class UTMToOdom():
 
         print(self.delta)
 
-if __name__ == '__main__':
-    rospy.init_node('utm_to_odom_node', anonymous=True)
+
+if __name__ == "__main__":
+    rospy.init_node("utm_to_odom_node", anonymous=True)
 
     utm_to_odom = UTMToOdom()
 
--- robomech_nav/scripts/teleop_patrick.py
+++ robomech_nav/scripts/teleop_patrick.py
@@ -4,60 +4,71 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
 
+
 class PatrickTeleop:
 
     def __init__(self):
-        rospy.init_node('patrick_teleop', anonymous=True)
-        self.twistMsg = Twist()
-        self.joyResults = Joy()
-        self.joyAxis = None
-        self.joyButtons = None
+        rospy.init_node("patrick_teleop", anonymous=True)
+        self.twist_msg = Twist()
+        self.joy_results = Joy()
+        self.joy_axis = None
+        self.joy_buttons = None
         self.rate = rospy.Rate(10)
-        
+
         self.load_ros_params()
-      
-        self.joySubscriber = rospy.Subscriber(self._joy_topic,Joy,self.joyCallback)
-        self.twistPublisher = rospy.Publisher(self._cmd_managed_topic, Twist, queue_size=0)
+
+        self.joy_subscriber = rospy.Subscriber(self._joy_topic, Joy, self.joy_callback)
+        self.twist_publisher = rospy.Publisher(
+            self._cmd_managed_topic, Twist, queue_size=0
+        )
 
         self.drive_factor = 1.0
         self.turn_factor = 3.0
 
+        self.pub_twist_cmd = False
+
         while not rospy.is_shutdown():
-            self.twistMsgConstructor()
-            self.twistPublisher.publish(self.twistMsg)
+            self.twist_msg_constructor()
+            if self.pub_twist_cmd:
+                self.twist_publisher.publish(self.twist_msg)
             self.rate.sleep()
-    
+
     def load_ros_params(self):
         # Load topic names into params
-        self._joy_topic = rospy.get_param('joy_topic')
-        self._cmd_managed_topic = rospy.get_param('cmd_vel_managed_topic')
-        
-        print ("Subscribing to: ", self._joy_topic)
-        print ("Publishing  to: ", self._cmd_managed_topic)
-        
-    def twistMsgConstructor(self):
-        if self.joyAxis:
-            self.twistMsg.linear.x = (self.joyAxis[1])*self.drive_factor
-            
-            self.twistMsg.angular.z = (self.joyAxis[3])*self.turn_factor
-        if self.joyButtons:
-            if self.joyButtons[3]:
-                self.drive_factor = min(3, self.drive_factor+0.5)
+        self._joy_topic = rospy.get_param("joy_topic")
+        self._cmd_managed_topic = rospy.get_param("cmd_vel_managed_topic")
+
+        print("Subscribing to: ", self._joy_topic)
+        print("Publishing  to: ", self._cmd_managed_topic)
+
+    def twist_msg_constructor(self):
+        if self.joy_axis:
+            self.twist_msg.linear.x = (self.joy_axis[1]) * self.drive_factor
+            self.twist_msg.angular.z = (self.joy_axis[3]) * self.turn_factor
+        if self.joy_buttons:
+            if self.joy_buttons[3]:
+                self.drive_factor = min(3, self.drive_factor + 0.5)
                 print("Drive Factor: ", self.drive_factor)
-            elif self.joyButtons[0]:
-                self.drive_factor = max(0, self.drive_factor-0.5)
+            elif self.joy_buttons[0]:
+                self.drive_factor = max(0, self.drive_factor - 0.5)
                 print("Drive Factor: ", self.drive_factor)
-            elif self.joyButtons[1]:
-                self.turn_factor = min(6, self.turn_factor+0.5)
+            elif self.joy_buttons[1]:
+                self.turn_factor = min(6, self.turn_factor + 0.5)
                 print("Turn Factor: ", self.turn_factor)
-            elif self.joyButtons[2]:
-                self.turn_factor = max(4, self.turn_factor-0.5)
+            elif self.joy_buttons[2]:
+                self.turn_factor = max(4, self.turn_factor - 0.5)
                 print("Turn Factor: ", self.turn_factor)
 
-    def joyCallback(self,data):
-        self.joyResults = data
-        self.joyAxis = self.joyResults.axes
-        self.joyButtons = self.joyResults.buttons
+            if self.joy_buttons[5]:
+                self.pub_twist_cmd = True
+            else:
+                self.pub_twist_cmd = False
 
-if __name__ == '__main__':
+    def joy_callback(self, data):
+        self.joy_results = data
+        self.joy_axis = self.joy_results.axes
+        self.joy_buttons = self.joy_results.buttons
+
+
+if __name__ == "__main__":
     PatrickTeleop()
